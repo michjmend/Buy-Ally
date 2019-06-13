7a7ad9109f50515215275348b6ff8c360c4e7131
Kristen De La Rosa <delarosa.kristen@gmail.com>
2019-06-12 21:52:50 -0400

    restored navbar

--- public/js/postblock.js
+++ public/js/postblock.js
@@ -1,81 +1,23 @@
 $(document).ready(function () {
-  var postbutton = $("#formSubmit");
-  var itemInput = $("#item");
-  var reviewInput = $("#reviews");
-  var priceInput = $("#price");
-  var imageInput = $("#uploadFile");
-  var brandInput = $("#brand");
-  var urlInput = $("#url");
-  var categoryInput = $("select#departmentCategory");
+  $.get("/api/category", function (dbcategory) {
+    console.log(dbcategory);
+    var options = "<option value=''>Category</option>";
 
-  // postbutton.on("click", function(event) {
-  //   event.preventDefault();
-  //   var userData = {
-  //     picture: imageInput,
-  //     item: itemInput.val(),
-  //     review: reviewInput.val(),
-  //     price: priceInput.val().trim(),
-  //     brand: brandInput.val().trim(),
-  //     url: urlInput.val().trim(),
-  //     category: categoryInput.find(":selected").val()
-  //   };
-  //   // if (!userData.name || !userData.review || !userData.price) || (!userData.category) {
-  //   //   return;
-  //   // }
-  //   console.log(userData);
-
-  //   createNewPost(
-  //     userData.item,
-  //     userData.review,
-  //     userData.price,
-  //     userData.brand,
-  //     userData.url,
-  //     userData.category
-  //   );
-
-  $("#newPost").submit(function (e) {
-    alert("Err");
-    e.preventDefault();
-    var formData = new FormData(this);
-    console.log(formData)
-    $.ajax({
-      type: "POST",
-      url: "/api/product",
-      data: formData,
-      processData: false,
-      contentType: false,
-      success: function (r) {
-        window.location.replace(r)
-      },
-      error: function (e) {
-        console.log("some error", e);
-      }
+    dbcategory.forEach(function (option) {
+      options += "<option data-id='" + option.id + "' value='" + option.id + "'>" + option.categoryname + "</option>";
     });
-    return false;
+    $("#departmentCategory").html(options)
   });
-});
 
-// function createNewPost(name, review, picture, price, brand, url, category) {
-//   // ajax call to our API;
-//   $.ajax({
-//     type: "POST",
-//     url: "/api/product",
-//     // enctype: "multipart/form-data",
-//     processData: false,
-//     contentType: false,
-//     cache: false,
-//     data: {
-//       name: name,
-//       review: review,
-//       picture: picture,
-//       price: price,
-//       brand: brand,
-//       url: url,
-//       CategoryId: category,
-//       UserId: 1
-//     }
-//   }).done(data => {
-//     window.location.replace(data);
-//     console.log("hello");
-//   });
-// }
+  function load_json_data(id) {
+    var filter = '';
+    $.getJSON(Category.json, function (data) {
+      filter += '<option value="">Select ' + id + '</option>';
+      $.each(data, function (key, value) {
+        filter += '<option value="">Select ' + id + '> ' + value.categoryname + '</option>';
+      });
+      $('#Category').html(filter);
+
+    });
+  }
+});
